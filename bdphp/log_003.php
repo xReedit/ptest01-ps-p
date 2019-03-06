0ec269a97a962fedf3d4361dbf7ee6bbec112845
Marcelo <xReedit@gmail.com>
2019-03-06 06:57:11 -0500

    proximos al despegue

--- bdphp/log_003.php
+++ bdphp/log_003.php
@@ -11,12 +11,22 @@
 
 	$op = $_GET['op'];	
     switch ($op) {
+		case '0':// prepar variables					
+				$_SESSION['ido']=$_POST['o'];
+				$_SESSION['idsede']=$_POST['s'];		
+				
+				// ip local
+				$sql="select ip_server_local from sede where idsede=".$_SESSION['idsede'];
+				$ipLocal=$bd->xDevolverUnDato($sql);
+				print "http://".$ipLocal;
+				return;
+			break;
 		case '1': //registrar impresion
 			$detalle_json = $_POST['datos'];
 			$idprint_server_estructura = $_POST['idprint_server_estructura'];
 			$tipo = $_POST['tipo'];
-			$sql="INSERT INTO print_server_detalle (idorg, idsede, idusuario, idprint_server_estructura, descripcion_doc, fecha, hora, detalle_json) 
-											values (".$_SESSION['ido'].",".$_SESSION['idsede'].",".$_SESSION['idusuario'].",".$idprint_server_estructura.", '".$tipo."', DATE_FORMAT(now(),'%d/%m/%Y'), DATE_FORMAT(now(),'%H:%i:%s'),'".$detalle_json."')";
+			$sql="INSERT INTO print_server_detalle (idorg, idsede, idprint_server_estructura, descripcion_doc, fecha, hora, detalle_json) 
+											values (".$_SESSION['ido'].",".$_SESSION['idsede'].",".$idprint_server_estructura.", '".$tipo."', DATE_FORMAT(now(),'%d/%m/%Y'), DATE_FORMAT(now(),'%H:%i:%s'),'".$detalle_json."')";
 			
 			// echo $sql;
 			$bd->xConsulta($sql);
@@ -24,20 +34,26 @@
 		case '2': // buscar documentos no imprimidos
 			$UltimoId=$_POST['ultimoId'];
 			if ( $UltimoId!='' ) { $UltimoId=' and psd.idprint_server_detalle>'.$UltimoId.' '; }
-			$sql="SELECT psd.*, pse.estructura_json, pse.nom_documento, u.nombres as nomUs
+			// $sql="SELECT psd.*, pse.estructura_json, pse.nom_documento, u.nombres as nomUs
+			// 			FROM print_server_detalle as psd
+			// 				INNER JOIN print_server_estructura as pse on pse.idprint_server_estructura = psd.idprint_server_estructura
+			// 				INNER JOIN usuario as u on u.idusuario = psd.idusuario
+			// 		WHERE (psd.idorg=".$_SESSION['ido']." and psd.idsede=".$_SESSION['idsede']." and psd.impreso=0) and psd.estado=0 ".$UltimoId." ORDER BY psd.idprint_server_detalle DESC";
+			
+			$sql="SELECT psd.*, pse.estructura_json, pse.nom_documento
 						FROM print_server_detalle as psd
-							INNER JOIN print_server_estructura as pse on pse.idprint_server_estructura = psd.idprint_server_estructura
-							INNER JOIN usuario as u on u.idusuario = psd.idusuario
+							INNER JOIN print_server_estructura as pse on pse.idprint_server_estructura = psd.idprint_server_estructura							
 					WHERE (psd.idorg=".$_SESSION['ido']." and psd.idsede=".$_SESSION['idsede']." and psd.impreso=0) and psd.estado=0 ".$UltimoId." ORDER BY psd.idprint_server_detalle DESC";
+			
 			$bd->xConsulta($sql);
 			break;
 		case '201': //verificar si hay nuevos registros
-			$UltimoId=$_POST['ultimoId'];
+			$UltimoId=$_GET['u'];
 			if ( $UltimoId!='' ) { $UltimoId=' and idprint_server_detalle>'.$UltimoId.' '; }
 
 			$sql="SELECT MAX(idprint_server_detalle) FROM print_server_detalle
 						where (idorg=".$_SESSION['ido']." and idsede=".$_SESSION['idsede']." and impreso=0)".$UltimoId;
-			
+						
 			$numero_pedidos_actual=$bd->xDevolverUnDato($sql);
 			echo "retry: 2000\n"."data:".$numero_pedidos_actual."\n\n";
 			ob_flush();
